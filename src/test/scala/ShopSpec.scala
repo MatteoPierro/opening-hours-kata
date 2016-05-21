103b8b9cbb3c4c65d8f41aaa49340e3226fd59da
MatteoPierro <pierromatteo@gmail.com>
2016-05-21 18:56:41 +0200

    adds test case to nextOpeningDay

--- src/test/scala/ShopSpec.scala
+++ src/test/scala/ShopSpec.scala
@@ -44,22 +44,31 @@ class ShopSpec extends FlatSpec with Matchers {
 
     shop nextOpeningDate wednesday shouldBe fridayMorning
   }
+
+  it should "have current day as opening day if hour is before opening hour" in {
+    val wednesdayBeforeOpeningDay: ZonedDateTime = "2016-05-11T06:22:11.824Z"
+    val wednesdayAtOpeningDay: ZonedDateTime = "2016-05-11T08:00:00.000Z"
+
+    shop nextOpeningDate wednesdayBeforeOpeningDay shouldBe wednesdayAtOpeningDay
+  }
 }
 
 class Shop(openingDays: List[DayOfWeek], openingHours: (LocalTime, LocalTime)) {
-  def nextOpeningDate(date: ZonedDateTime): ZonedDateTime = {
-    if(openingDays.contains(date.getDayOfWeek) && !date.toLocalTime.isAfter(openingHours._1)) {
-      ZonedDateTime.of(date.toLocalDate, openingHours._1, date.getZone)
+  def nextOpeningDate(date: ZonedDateTime): ZonedDateTime =
+    isAnOpeningDay(date) && isNotAfterOpeningHour(date.toLocalTime) match {
+      case true =>
+        ZonedDateTime.of(date.toLocalDate, openingHours._1, date.getZone)
+
+      case _ =>
+        val nextDay = date.plusDays(1)
+        nextOpeningDate(ZonedDateTime.of(nextDay.toLocalDate, openingHours._1, nextDay.getZone))
     }
-    else{
-      val nextDate = date.plusDays(1)
-      nextOpeningDate(ZonedDateTime.of(nextDate.toLocalDate, openingHours._1, nextDate.getZone))
-    }
-  }
 
   def isOpenOn(date: ZonedDateTime): Boolean = isAnOpeningDay(date) && isAnOpeningHour(date.toLocalTime)
 
   private def isAnOpeningDay(date: ZonedDateTime): Boolean = openingDays contains date.getDayOfWeek
 
   private def isAnOpeningHour(hour: LocalTime): Boolean = !hour.isBefore(openingHours._1) && !hour.isAfter(openingHours._2)
+
+  private def isNotAfterOpeningHour(hour: LocalTime): Boolean = !hour.isAfter(openingHours._1)
 }
