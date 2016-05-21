1494ccdf26f786d5bcca26ee115dd90f7366bb5a
MatteoPierro <pierromatteo@gmail.com>
2016-05-21 17:46:05 +0200

    adds test case 'at opening hour'

--- src/test/scala/ShopSpec.scala
+++ src/test/scala/ShopSpec.scala
@@ -7,40 +7,44 @@ class ShopSpec extends FlatSpec with Matchers {
 
   implicit def stringToDate(string: String) = ZonedDateTime parse string
 
-  val openingDay = List(MONDAY, WEDNESDAY, FRIDAY)
+  val openingDays = List(MONDAY, WEDNESDAY, FRIDAY)
 
-  val openingTime = (LocalTime.of(8, 0), LocalTime.of(16, 0))
+  val openingHours = (LocalTime.of(8, 0), LocalTime.of(16, 0))
 
-  it should "be open in an opening day" in {
-    val shop = new Shop(openingDay, openingTime)
+  "Shop" should "be open in an opening day" in {
+    val shop = new Shop(openingDays, openingHours)
     val wednesday = "2016-05-11T12:22:11.824Z"
 
     shop isOpenOn wednesday shouldBe true
   }
 
   it should "be closed in an closing day" in {
-    val shop = new Shop(openingDay, openingTime)
+    val shop = new Shop(openingDays, openingHours)
     val thursday = "2016-05-12T12:22:11.824Z"
 
     shop isOpenOn thursday shouldBe false
   }
 
   it should "be closed in an closing hour" in {
-    val shop = new Shop(openingDay, openingTime)
+    val shop = new Shop(openingDays, openingHours)
     val wednesdayBeforeOpeningHour = "2016-05-11T07:22:11.824Z"
 
     shop isOpenOn wednesdayBeforeOpeningHour shouldBe false
   }
+
+  it should "be open at opening hour" in {
+    val shop = new Shop(openingDays, openingHours)
+    val wednesday = "2016-05-11T08:00:00Z"
+
+    shop isOpenOn wednesday shouldBe true
+  }
 }
 
-class Shop(openingDay: List[DayOfWeek], openingTime: (LocalTime, LocalTime)) {
+class Shop(openingDays: List[DayOfWeek], openingHours: (LocalTime, LocalTime)) {
 
-  def isOpenOn(date: ZonedDateTime): Boolean = isAnOpeningDay(date) && isAnOpeningHour(date)
+  def isOpenOn(date: ZonedDateTime): Boolean = isAnOpeningDay(date) && isAnOpeningHour(date.toLocalTime)
 
-  def isAnOpeningDay(date: ZonedDateTime): Boolean = openingDay contains date.getDayOfWeek
+  private def isAnOpeningDay(date: ZonedDateTime): Boolean = openingDays contains date.getDayOfWeek
 
-
-  def isAnOpeningHour(date: ZonedDateTime): Boolean = {
-    date.toLocalTime.isAfter(openingTime._1) && date.toLocalTime.isBefore(openingTime._2)
-  }
+  private def isAnOpeningHour(hour: LocalTime): Boolean = !hour.isBefore(openingHours._1) && !hour.isAfter(openingHours._2)
 }
