71477126badae54f4be52c6efa997c258291d4d5
MatteoPierro <pierromatteo@gmail.com>
2016-05-25 20:59:12 +0200

    removes pattern matching

--- src/main/scala/Shop.scala
+++ src/main/scala/Shop.scala
@@ -1,15 +1,15 @@
 import java.time.{DayOfWeek, LocalTime, ZonedDateTime}
 
 class Shop(openingDays: List[DayOfWeek], openingHours: (LocalTime, LocalTime)) {
-  def nextOpeningDate(date: ZonedDateTime): ZonedDateTime =
-    isAnOpeningDay(date) && isNotAfterOpeningHour(date.toLocalTime) match {
-      case true =>
-        ZonedDateTime.of(date.toLocalDate, openingHours._1, date.getZone)
 
-      case _ =>
-        val nextDay = date.plusDays(1)
-        nextOpeningDate(ZonedDateTime.of(nextDay.toLocalDate, openingHours._1, nextDay.getZone))
+  def nextOpeningDate(date: ZonedDateTime): ZonedDateTime = {
+    if (isAnOpeningDay(date) && isNotAfterOpeningHour(date.toLocalTime)) {
+      return ZonedDateTime.of(date.toLocalDate, openingHours._1, date.getZone)
     }
+    
+    val nextDay = date.plusDays(1)
+    nextOpeningDate(ZonedDateTime.of(nextDay.toLocalDate, openingHours._1, nextDay.getZone))
+  }
 
   def isOpenOn(date: ZonedDateTime): Boolean = isAnOpeningDay(date) && isAnOpeningHour(date.toLocalTime)
 
